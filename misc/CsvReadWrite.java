33cfe780ce3eeb1a4b78cb4a41ff0c5e444b4a51
jothpravin <jothipravin5@gmail.com>
2024-02-16 18:19:26 +0530

    feb 16 files

--- misc/CsvReadWrite.java
+++ misc/CsvReadWrite.java
@@ -4,8 +4,8 @@ public class CsvReadWrite
 {
 	public static void main(String []args)
 	{
-		try(BufferedReader br = new BufferedReader(new FileReader("/home/zoho/jothipravin/write.csv"));
-			PrintWriter pw = new PrintWriter("/home/zoho/jothipravin/newcopy.csv"))
+		try(BufferedReader br = new BufferedReader(new FileReader("/home/zoho/jothipravin/Zoho_Training_Files/misc/write.csv"));
+			PrintWriter pw = new PrintWriter("/home/zoho/jothipravin/Zoho_Training_Files/misc/newcopy1.csv"))
 		{
 			String line;
 			while((line = br.readLine()) != null)
